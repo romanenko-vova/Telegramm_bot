4684c13aac59f1107dc64c4a32787249b0a85821
romanenko_vova <bonyshd@gmail.com>
2025-03-09 16:39:07 +0300

    Я тебе поправил статистику

--- rate.py
+++ rate.py
@@ -19,10 +19,19 @@ async def my_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [InlineKeyboardButton("Назад", callback_data="back")],
     ]
     markup = InlineKeyboardMarkup(keyboard)
-
-async def knb_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await query.edit_message_text("Ваша статистика:", reply_markup=markup)
+    await query.edit_message_text("Ваша статистика", reply_markup=markup)
 
     return RATE
+
 async def knb_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+    query = update.callback_query
+    await query.answer()
+    
+    keyboard = [
+        [InlineKeyboardButton("статистика knz", callback_data="knz_state")],
+        [InlineKeyboardButton("статистика bak", callback_data="bak_state")],
+        [InlineKeyboardButton("Назад", callback_data="back")],
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text("Статистика КНБ", reply_markup=markup)
+    return RATE
